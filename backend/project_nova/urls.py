494c1a48006a8ed9ab53afa999f19849c6d537be
Ananthu Viswanath <ananthuviswanath829@gmail.com>
2022-10-09 21:25:11 +0100

    Sign In page ui

--- backend/project_nova/urls.py
+++ backend/project_nova/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
+from django.urls import path, include, re_path #Ananthu
+from django.views.generic import TemplateView #Ananthu
 urlpatterns = [
     path('api/', include('app_nova.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
