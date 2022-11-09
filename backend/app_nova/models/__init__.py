ebf5ab3ed8d09387bf2bbd9125879de215310c13
Ananthu Viswanath <ananthuviswanath829@gmail.com>
2022-11-09 22:53:19 +0000

    friend table

--- backend/app_nova/models/__init__.py
+++ backend/app_nova/models/__init__.py
@@ -6,4 +6,6 @@ from .emailverification import EmailVerificationCode #Ananthu
 from .skill import Skill #Ananthu
 from .userskill import UserSkill #Ananthu
 
+from .friends import Friends #Ananthu
+
 from .log import Log #Ananthu
---
+++ backend/app_nova/models/friends.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+##Table for saving Friends.
+#Author-Ananthu
+class Friends(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    is_accepted = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
